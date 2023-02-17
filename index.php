3956d92bc352cf1bbd5441219a46104aec27b0c7
AndreaMinelli <minelliandrea94@gmail.com>
2023-02-17 15:15:04 +0100

    Creata class Genre

--- index.php
+++ index.php
@@ -27,3 +27,23 @@ class Movie
         return "$this->vote/10";
     }
 }
+
+class Genre
+{
+    public $name;
+
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+}
+
+$fantasy = new Genre('Fantasy');
+$thriller = new Genre('Thriller');
+$distopico = new Genre('Distopico');
+
+$the_martians = new Movie('The Martians', 'en-EN', [$fantasy, $thriller], 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam itaque est recusandae, et corporis explicabo distinctio obcaecati modi similique ducimus eveniet, sunt eaque minus nam aperiam? Necessitatibus nobis qui repellat.', '7');
+$fight_club = new Movie('Fight Club', 'en-US', [$distopico, $thriller], 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam itaque est recusandae, et corporis explicabo distinctio obcaecati modi similique ducimus eveniet, sunt eaque minus nam aperiam? Necessitatibus nobis qui repellat.', '9');
+
+var_dump($the_martians);
+var_dump($fight_club);
